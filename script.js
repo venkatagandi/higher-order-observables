46a6619027364428594b34b245ef0cac286e7c5d
venkatagandi <venkata.gandi@gmail.com>
2018-08-20 04:51:15 +0100

    added concat all changes.its mergeAll(1).

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const clickObservable = Rx.Observable.fromEvent(document, "click");
 
 const clockObservable = clickObservable
-  .map(click => Rx.Observable.interval(1000))
-  // .mergeAll();
-  .mergeAll(2);
+  .map(click => Rx.Observable.interval(1000).take(5))
+  // .mergeAll(1);
+  .concatAll();
 clockObservable.subscribe(x => console.log(x));
