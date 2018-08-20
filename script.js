7d166dcb65dc3a28a60a3fa3cfb92ca551fabbb4
venkatagandi <venkata.gandi@gmail.com>
2018-08-20 04:46:47 +0100

    added merge all changes

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const clickObservable = Rx.Observable.fromEvent(document, "click");
 
 const clockObservable = clickObservable
   .map(click => Rx.Observable.interval(1000))
-  .switch();
-
+  // .mergeAll();
+  .mergeAll(2);
 clockObservable.subscribe(x => console.log(x));
